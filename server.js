0c8d07b43eab4816dd32da2cf36373be07c479cb
unarunars <unr2@hi.is>
2020-10-25 21:29:51 +0000

    heroku fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
+require('make-promises-safe').abort = true
 
 //const app = express()
 app.use(bodyParser.json())
