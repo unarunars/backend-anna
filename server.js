7c1477456ba00bf97fbe1c760b3986f0da7c9ec0
unarunars <unr2@hi.is>
2020-11-10 19:54:06 +0000

    testa

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ let router = require('./app/routers/file.router.js');
 app.use('/', router);
  
 // Create a Server
-let port = env.PORT || 8080;
+let port = process.env.PORT || 8080;
 const server = app.listen(/*5432,'0.0.0.0',*/ port , '0.0.0.0',function () {
   let host = server.address().address;
   let port = server.address().port;
